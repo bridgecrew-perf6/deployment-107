1d8fc5607635f627bae3455bdbe8810d16c632a0
the-bob <adelbertureta@MacBookPro.station>
2022-05-09 09:55:41 +0100

    feat: db tunneller

--- tf-deployment/versions.tf
+++ tf-deployment/versions.tf
@@ -3,5 +3,27 @@ terraform {
     secret_suffix = "state"
     config_path   = "~/.kube/config"
   }
+  
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+
   required_version = "~> 1.1.8"
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
 }
